04fc0060e99ecb587b23b537449673edae3e9d53
Wan M. Wimberly <arwm4@Wans-Air.attlocal.net>
2022-04-30 21:38:36 -0400

    Add tes suite af

---
+++ src/App.test.js
@@ -0,0 +1,90 @@
+import { render, cleanup, rerender, screen } from '@testing-library/react'
+import Root from './containers/Root'
+import configureStore from './store/configureStore'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import users from './users.api.json'
+
+let store
+const {
+  users: [
+    { name: philip },
+    { name: turanga },
+    { name: bender },
+    { name: planet },
+    { name: john },
+    { name: zapp },
+    { name: kif }
+  ]
+} = users
+
+const regex = text => new RegExp(text)
+
+beforeEach(() => {
+  store = configureStore()
+})
+
+describe('200 OK', () => {
+  const server = setupServer(
+    rest.get('https://immense-bastion-95145.herokuapp.com/api/users', (req, res, ctx) => {
+      return res(ctx.json(users))
+    })
+  )
+
+  beforeAll(() => server.listen())
+  afterEach(() => server.resetHandlers())
+  afterAll(() => server.close())
+
+  test('renders users select list', async () => {
+    render(<Root store={store}/>)
+    const loading = screen.getByText(/Loading/i)
+    expect(loading).toBeInTheDocument()
+
+    const [
+      philipOption,
+      turangaOption,
+      benderOption,
+      planetOption,
+      johnOption,
+      zappOption,
+      kifOption
+    ] = await Promise.all([
+      screen.findByText(regex(philip)),
+      screen.findByText(regex(turanga)),
+      screen.findByText(regex(bender)),
+      screen.findByText(regex(planet)),
+      screen.findByText(regex(john)),
+      screen.findByText(regex(zapp)),
+      screen.findByText(regex(kif))
+    ])
+
+    expect(philipOption).toBeInTheDocument()
+    expect(turangaOption).toBeInTheDocument()
+    expect(benderOption).toBeInTheDocument()
+    expect(planetOption).toBeInTheDocument()
+    expect(johnOption).toBeInTheDocument()
+    expect(zappOption).toBeInTheDocument()
+    expect(kifOption).toBeInTheDocument()
+  })
+})
+
+describe('API error', () => {
+  const server = setupServer(
+    rest.get('https://immense-bastion-95145.herokuapp.com/api/users', (req, res, ctx) => {
+      return res(ctx.status(404), ctx.json())
+    })
+  )
+
+  beforeAll(() => server.listen())
+  afterEach(() => server.resetHandlers())
+  afterAll(() => server.close())
+
+  test('handles API errors', async () => {
+    render(<Root store={store}/>)
+    const loading = screen.getByText(/Loading/i)
+    expect(loading).toBeInTheDocument()
+
+    const error = await screen.findByText(/error/)
+    expect(error).toBeInTheDocument()
+  })
+})
--- src/actions/index.js
+++ src/actions/index.js
@@ -22,7 +22,20 @@ export const selectUsers = users => ({
 export const loadUsers = () => {
   return dispatch => {
     return fetch('https://immense-bastion-95145.herokuapp.com/api/users')
-        .then(response => response.json())
+        .then(response => {
+	  const { status, statusText } = response
+
+	  if (status >= 200 && status < 300) {
+	    return response.json()
+	  }
+
+	  var error = new Error(statusText)
+
+	  error.status = status
+	  error.response = response
+
+	  throw error
+	})
 	.then(data => dispatch(setUsers(data.users)))
 	.catch(error => dispatch(usersError(error)))
   }
